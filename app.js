61da11021d33219fd77e548bcc56a48214b1979c
hunferno <kone.rene@hotmail.fr>
2020-12-13 22:52:32 +0100

    Creation of setup files

---
+++ app.js
@@ -0,0 +1,62 @@
+require("dotenv").config();
+require("./config/mongo"); // database initial setup
+
+// base dependencies
+const express = require("express");
+const app = express();
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const hbs = require("hbs");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const MongoStore = require("connect-mongo")(session);
+const dev_mode = false;
+const logger = require("morgan");
+
+// config logger (pour debug)
+app.use(logger("dev"));
+
+// initial config
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static("public"));
+hbs.registerPartials(__dirname + "/views/partial");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+// SESSION SETUP
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    cookie: { maxAge: 600000 }, // in millisec
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection, // you can store session infos in mongodb :)
+      ttl: 24 * 60 * 60, // 1 day
+    }),
+    saveUninitialized: true,
+    resave: true,
+  })
+);
+
+// below, site_url is used in partials/shop_head.hbs to perform ajax request (var instead of hardcoded)
+app.locals.site_url = process.env.SITE_URL;
+
+app.use(flash());
+
+// CUSTOM MIDDLEWARES
+
+if (dev_mode === true) {
+  app.use(require("./middlewares/devMode")); // triggers dev mode during dev phase
+  app.use(require("./middlewares/debugSessionInfos")); // displays session debug
+}
+
+app.use(require("./middlewares/exposeLoginStatus"));
+app.use(require("./middlewares/exposeFlashMessage"));
+
+// routers
+// app.use("/", require("./routes/index"));
+// app.use("/", require("./routes/auth"));
+// app.use("/", require("./routes/dashboard_sneaker"));
+
+module.exports = app;
