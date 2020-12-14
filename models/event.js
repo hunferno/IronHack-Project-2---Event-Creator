8771f101d2042c80df17dff4fc99eb04fc80a4d0
hunferno <kone.rene@hotmail.fr>
2020-12-14 09:14:44 +0100

    Fist Day Of Project

---
+++ models/event.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const EventsSchema = new Schema(
+  {
+    title: String,
+    location: {
+      adress: String,
+      zipCode: String,
+      city: String,
+      country: String,
+    },
+    description: String,
+    maxParticipant: Number,
+    image: {
+      type: String,
+      default:
+        "https://res.cloudinary.com/dke1st5wz/image/upload/v1607328768/sample.jpg",
+    },
+    id_user: [{ type: Schema.Types.ObjectId, ref: "user" }],
+  },
+  { timestamps: true }
+);
+
+const EventModel = mongoose.model("event", EventsSchema);
+
+module.exports = EventModel;
+
+// ********To build my route*********
+// {{#each posts}}
+//     <li>
+//         {{ this.title }} by {{this.author.username}}
+//         <a href="/posts/{{this._id}}">Read more</a>
+//     </li>
+//   {{/each}}
