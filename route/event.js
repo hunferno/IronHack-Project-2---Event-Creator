2a089e0d2820cecae35e5e1f58076ba127632c57
hunferno <kone.rene@hotmail.fr>
2020-12-15 15:21:39 +0100

    test

--- route/event.js
+++ route/event.js
@@ -22,9 +22,64 @@ router.get("/", async (req, res, next) => {
 });
 // *************End***********
 
+// *************Display allEvent in allEvents Page***********
+router.get("/allEvent", async (req, res, next) => {
+  try {
+    const allEvents = await EventModel.find();
+    res.render("allEvent", { allEvents });
+  } catch (error) {
+    next(error);
+  }
+});
+// *************End***********
+
+// *************Create Event***********
+router.post("/events_manage/create", async (req, res, next) => {
+  try {
+    if (req.file) {
+      req.body.image = req.file.path;
+    }
+    await EventModel.create(req.body);
+    res.redirect("/allEvent");
+  } catch (error) {
+    next(error);
+  }
+});
+// *************End***********
+
+// *************Delete Event***********
+router.get("/events_manage/delete/:id", async (req, res, next) => {
+  try {
+    await EventModel.findByIdAndRemove(req.params.id);
+    res.redirect("/allEvent");
+  } catch (error) {
+    next(error);
+  }
+});
+// *************End***********
+
+// *************Render Update Event page***********
+router.get("/events_manage/update/:id", async (req, res, next) => {
+  try {
+    const updateEvent = await EventModel.findById(req.params.id);
+    res.render("", updateEvent);
+  } catch (error) {
+    next(error);
+  }
+});
+// *************End***********
+
+// *************Update Event***********
+router.post("/events_manage/update/:id", async (req, res, next) => {
+  try {
+    await EventModel.findById(req.params.id, req.body);
+    res.redirect("/allEvent");
+  } catch (error) {
+    next(error);
+  }
+});
+// *************End***********
+
 module.exports = router;
 
 // const ev.find().populate("id_user")
-// const user
-
-// var q = models.Post.find({published: true}).sort({'date': -1}).limit(20);
