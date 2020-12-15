aa41e0bd4fd6db2fb409cbc407c0fbd2d458ab05
hunferno <kone.rene@hotmail.fr>
2020-12-15 23:33:09 +0100

    Adding some routes

--- route/index.js
+++ route/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const router = new express.Router();
+const EventModel = require("./../models/event");
 
-router.get("/allEvents", (req, res, next) => {
-  res.render("allEvents");
+// *************Feeding Carousel in Home Page***********
+router.get("/", async (req, res, next) => {
+  try {
+    const carouselImg = await EventModel.find()
+      .sort({ createdAt: -1 })
+      .limit(9);
+    res.render("homepage", { carouselImg });
+  } catch (error) {
+    next(error);
+  }
 });
+// *************End***********
 
 module.exports = router;
