40ad96552d4f3561604c9f153d6f3993891c86ed
Milande Teisserenc bonesteve <milandejf@gmail.com>
2020-12-15 21:01:32 +0100

    done

--- route/dashboard.js
+++ route/dashboard.js
@@ -4,7 +4,54 @@ const EventModel = require("./../models/event");
 const UserModel = require("./../models/user");
 
 router.get("/dashboard", (req, res) => {
-    res.render("dashboard");
+  res.render("dashboard", {
+    events: [
+        {
+            image:
+              "https://res.cloudinary.com/dqwbskuge/image/upload/v1607960794/allef-vinicius-DmUbkltYsKI-unsplash_u0hy1a.jpg",
+            title: "test",
+            description:
+              "Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.",
+          },
+          {
+            image:
+              "https://res.cloudinary.com/dqwbskuge/image/upload/v1607960794/allef-vinicius-DmUbkltYsKI-unsplash_u0hy1a.jpg",
+            title: "test",
+            description:
+              "Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.",
+          },
+          {
+            image:
+              "https://res.cloudinary.com/dqwbskuge/image/upload/v1607960794/allef-vinicius-DmUbkltYsKI-unsplash_u0hy1a.jpg",
+            title: "test",
+            description:
+              "Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.",
+          },
+    ],
+    myevents: [
+        {
+            image:
+              "https://res.cloudinary.com/dqwbskuge/image/upload/v1607960794/allef-vinicius-DmUbkltYsKI-unsplash_u0hy1a.jpg",
+            title: "lol",
+            description:
+              "the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.",
+          },
+          {
+            image:
+              "https://res.cloudinary.com/dqwbskuge/image/upload/v1607960794/allef-vinicius-DmUbkltYsKI-unsplash_u0hy1a.jpg",
+            title: "test",
+            description:
+              "Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.",
+          },
+          {
+            image:
+              "https://res.cloudinary.com/dqwbskuge/image/upload/v1607960794/allef-vinicius-DmUbkltYsKI-unsplash_u0hy1a.jpg",
+            title: "test",
+            description:
+              "Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.Render view with the given options and optional callback fn. When a callback function is given a response will not be made automatically, otherwise a response of 200 and text/html is given.",
+          },
+    ],
   });
+});
 
-module.exports = router; 
+module.exports = router;
